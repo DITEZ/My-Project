1b3e3138d5928f47bcf294dddcf7316f7b8670f0
Teddy Nyagaka <teddynyagaka@gmail.com>
2023-11-21 14:16:07 +0300

    splashscreen

--- finalproject/lib/main.dart
+++ finalproject/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:finalproject/views/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'views/screens/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(),
     );
   }
 }
--- finalproject/lib/views/screens/splashscreen.dart
+++ finalproject/lib/views/screens/splashscreen.dart
@@ -1,16 +1,40 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:finalproject/main.dart';
+import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class Splash extends StatefulWidget {
+ const Splash({Key? key}) : super(key: key);
 
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
+ @override
+ _SplashState createState() => _SplashState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+class _SplashState extends State<Splash> {
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _navigatetohome();
   }
+   _navigatetohome()async{
+    await Future.delayed(Duration(milliseconds: 1500),(){});
+   // Navigator.pushReplacement(
+     // context, MaterialPageRoute(builder: (context)=> MyHomePage(title: title)) )
+   }
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     body: Center(
+       child: Container(
+         child: Text(
+           'splashscreen',
+           style: TextStyle(
+             fontSize: 24,
+             fontWeight: FontWeight.bold,
+           ),
+         ),
+       ),
+     ),
+   );
+ }
 }
