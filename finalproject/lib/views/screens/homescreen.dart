e3d3f03f177135fa660c97f3ac1e766c78ee9d98
Teddy Nyagaka <teddynyagaka@gmail.com>
2024-02-02 22:15:38 +0300

    modified homescreen

--- finalproject/lib/views/screens/homescreen.dart
+++ finalproject/lib/views/screens/homescreen.dart
@@ -1,11 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyHomescreen extends StatelessWidget {
+   const MyHomescreen({Key? key, required this.title}) : super(key: key);
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text(this.title),),
+      body: Center(child: Text('HIKING APP')),
+      );
   }
 }
